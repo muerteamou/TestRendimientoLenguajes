6996fa50cd399eaa09a89d0e17f1d5dcc4869da5
muerteamou <alejandropinero@gmail.com>
2025-01-22 05:11:17 +0100

    Test java complete

--- Java/prueba.java
+++ Java/prueba.java
@@ -1,29 +1,74 @@
 public class prueba {
+
     public static void main(String[] args) {
+
+        // Constantes para el número de iteraciones y el factor de multiplicación
+        final int ITERATIONS = 1_000_000_000, factor = 9999; // 1.000 millonesde iteraciones y factor de multiplicación
+
+        // Variables para almacenar los resultados de las operaciones
+        int multIntResult = 0;
+        double multDoubleResult = 0, divResult = 0;
+
+        double timeIni = 0, timeFin = 0, timeInt = 0, timeDou = 0, timeDiv = 0;
+
+        System.out.println("Test de rendimiento de java - Manejo de bucles y operaciones matemáticas");
+
+        System.out.println("Comienza la multiplicación de enteros");
+
+        iniciarCuentaAtras();
+
         // Capturar el tiempo inicial en milisegundos
-        long time1 = System.currentTimeMillis();
+        timeIni = System.currentTimeMillis();
 
         // Primera operación: multiplicación
-        for (int i = 0; i < 1_000_000_000; i++) {
-            int multiplicacion = 2000 * 55555;
+        for (int i = 0; i < ITERATIONS; i++) {
+            multIntResult = factor * 5;
         }
-        long time2 = System.currentTimeMillis();
+        // Capturar el tiempo final en milisegundos
+        timeFin = System.currentTimeMillis();
+        timeInt = (timeFin - timeIni) / 1000;
+
+        System.out.println("Comienza la multiplicación de decimales");
+
+        iniciarCuentaAtras();
+        // Capturar el tiempo inicial en milisegundos
+        timeIni = System.currentTimeMillis();
 
         // Segunda operación: multiplicación con decimales
-        for (int i = 0; i < 1_000_000_000; i++) {
-            double multiplicacion = 9999 * 0.5;
+        for (int i = 0; i < ITERATIONS; i++) {
+            multDoubleResult = factor * 0.5;
         }
-        long time3 = System.currentTimeMillis();
+        // Capturar el tiempo final en milisegundos
+        timeFin = System.currentTimeMillis();
+        timeDou = (timeFin - timeIni) / 1000;
 
-        // Tercera operación: división 
-        for (int i = 0; i < 1_000_000_000; i++) {
-            double division = 9999 / 2.0;
+        System.out.println("Comienza la división");
+        iniciarCuentaAtras();
+        // Capturar el tiempo inicial en milisegundos
+        timeIni = System.currentTimeMillis();
+
+        // Tercera operación: división
+        for (int i = 0; i < ITERATIONS; i++) {
+            divResult = factor / 2.0;
         }
-        long time4 = System.currentTimeMillis();
+        timeFin = System.currentTimeMillis();
+
+        timeDiv = (timeFin - timeIni) / 1000;
 
         // Imprimir los resultados
-        System.out.println("El tiempo de ejecución de multiplicar enteros es: " + (time2 - time1) / 1000.0 + " segundos");
-        System.out.println("El tiempo de ejecución de multiplicar decimales es: " + (time3 - time2) / 1000.0 + " segundos");
-        System.out.println("El tiempo de ejecución de dividir es: " + (time4 - time3) / 1000.0 + " segundos");
+        System.out.println("El tiempo de ejecución de multiplicar enteros es: " + timeInt + " segundos. Resultado de la operación: " + multIntResult);
+        System.out.println("El tiempo de ejecución de multiplicar decimales es: " + timeDou + " segundos. Resultado de la operación: " + multDoubleResult);
+        System.out.println("El tiempo de ejecución de dividir es: " + timeDiv + " segundos. Resultado de la operación: " + divResult);
+    }
+
+    public static void iniciarCuentaAtras() {
+        for (int i = 3; i > 0; i--) {
+            System.out.println(i);
+            try {
+                Thread.sleep(1000); // Esperar 1 segundo entre cada número
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
